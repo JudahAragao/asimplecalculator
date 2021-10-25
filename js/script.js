5c50db02a1add7930a8c37cf46e81a88b65c1937
Judah Ben-Hur F de Aragão <jbhfaragao@cgm.pma.rima>
2021-10-25 10:30:18 -0300

    calculator V2.0 math operation with only two variables

--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
+var operations = []
+
+const operators = {
+    "+": function(prefix, suffix){return prefix + suffix},
+    "-": function(prefix, suffix){return prefix - suffix},
+    "*": function(prefix, suffix){return prefix * suffix},
+    "/": function(prefix, suffix){return prefix / suffix},
+}
+
 function insert(num){
     document.form.textview.value += num
+    operations.push(num)
 }
 
 function equal(){
-    var exp = document.form.textview.value
+    var prefix
+    var operator
+    var suffix
+    document.form.textview.value = null
+    operations.forEach(element => {
+        if (typeof(element) === "string") {
+            operator = element
+            if (operations.indexOf(element)) {
+                prefix = operations.slice(0,operations.indexOf(element))
+                suffix = operations.slice(operations.indexOf(element)+1)
+                result = operators[operator](prefix,suffix)
+                document.form.textview.value = `${prefix}` + `${operator}` + `${suffix}` + '=' + `${result}`
+            }
+        }
+    });
 
-    if(exp){
-        document.form.textview.value = eval(exp)
-    }
 }
 
 function clean(){
     document.form.textview.value = ""
+    while(operations.length) {
+        operations.pop()
+    }
 }
 
 function backspace(){
     var exp = document.form.textview.value
     document.form.textview.value = exp.substring(0, exp.length-1)
+    operations.pop()
 }
